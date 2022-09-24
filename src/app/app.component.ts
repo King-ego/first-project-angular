f34a25a217ff47f6a8a3c31b35c84d43621419f4
Diego Santos <diegosantos@gmail.com>
2022-09-24 08:54:43 -0300

    data for child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'first-project-angular';
+  person = { name: 'diego', age: '25' }
+  andress = 'Rua'
+  title = 'first-project-angular'
 }
