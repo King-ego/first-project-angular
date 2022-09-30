4d7c7770f8f5e07547c97ef52ea4326d2da8187f
Diego Santos <diegosantos@gmail.com>
2022-09-30 12:57:56 -0300

    two way data bind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FirstComponentComponent } from './components/first-component/first-component.component';
-import { ParentDataComponent } from './components/parent-data/parent-data.component';
-import { DirectivesComponent } from './components/directives/directives.component';
-import { IfRenderComponent } from './components/if-render/if-render.component';
-import { EventsComponent } from './components/events/events.component';
-import { EmitterComponent } from './components/emitter/emitter.component';
-import { ChangeNumberComponent } from './components/change-number/change-number.component';
-import { ListRenderComponent } from './components/list-render/list-render.component';
-import { PipeComponent } from './components/pipe/pipe.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { FirstComponentComponent } from './components/first-component/first-component.component'
+import { ParentDataComponent } from './components/parent-data/parent-data.component'
+import { DirectivesComponent } from './components/directives/directives.component'
+import { IfRenderComponent } from './components/if-render/if-render.component'
+import { EventsComponent } from './components/events/events.component'
+import { EmitterComponent } from './components/emitter/emitter.component'
+import { ChangeNumberComponent } from './components/change-number/change-number.component'
+import { ListRenderComponent } from './components/list-render/list-render.component'
+import { PipeComponent } from './components/pipe/pipe.component'
+import { TwoWayBindComponent } from './components/two-way-bind/two-way-bind.component'
 
 @NgModule({
   declarations: [
@@ -24,13 +26,11 @@ import { PipeComponent } from './components/pipe/pipe.component';
     EmitterComponent,
     ChangeNumberComponent,
     ListRenderComponent,
-    PipeComponent
+    PipeComponent,
+    TwoWayBindComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
